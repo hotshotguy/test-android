e224de7cb82d3840fbf90baafeacdc1cf63bd441
Ivan Shamshurin <hotshotguy1982@gmail.com>
2013-09-15 02:34:50 +0400

    Added response activity

--- hh_app/src/com/example/hh_app/Activity1.java
+++ hh_app/src/com/example/hh_app/Activity1.java
@@ -1,28 +1,44 @@
 package com.example.hh_app;
 
+
+import org.w3c.dom.Text;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
-import android.text.Editable;
-import android.text.InputFilter;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Activity1 extends Activity {
+	private TextView date;
+	private DatePicker date_picker;
 
-    @Override
+	private int year;
+	private int month;
+	private int day;
+
+	static final int DATE_DIALOG_ID = 100;
+	public static final String EXTRA_MESSAGE = "com.example.hh_app.MESSAGE";
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-    	setContentView(R.layout.activity_activity1);
-	
+    	setContentView(R.layout.activity1_main);
+
     	Spinner spinnerSex = (Spinner) findViewById(R.id.spinnerSex);
-        
+
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.sex, android.R.layout.simple_spinner_dropdown_item);
@@ -31,7 +47,43 @@ public class Activity1 extends Activity {
         // Apply the adapter to the spinner
         spinnerSex.setAdapter(adapter);
         spinnerSex.setPromptId(R.string.prompt);
+
+        addTextVeiwListener();
     }
+
+    private void addTextVeiwListener() {
+		date = (TextView) findViewById(R.id.textAddBirth);
+
+		date.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				showDialog(DATE_DIALOG_ID);
+			}
+		});
+    }
+    @Override
+    protected Dialog onCreateDialog(int id) {
+
+    	switch (id) {
+    	case DATE_DIALOG_ID:
+    		// set date picker as current date
+    		return new DatePickerDialog(this, datePickerListener, year, month,day);
+    	}
+    	return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+    	// when dialog box is closed, below method will be called.
+    	public void onDateSet(DatePicker view, int selectedYear,int selectedMonth, int selectedDay) {
+    		year = selectedYear;
+    		month = selectedMonth;
+    		day = selectedDay;
+    		// set selected date into Text View
+    		date.setText(new StringBuilder().append(day)
+    				.append(".").append(month + 1).append(".").append(year).append(" г."));
+    	}
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,29 +93,148 @@ public class Activity1 extends Activity {
     }
 
     public void onClickNameText(View v) {
-    	LinearLayout editNameLayout = (LinearLayout) findViewById(R.id.NameLayoutEdit);
-    	EditText editNameText = (EditText) findViewById(R.id.editText1);
-    	editNameText.setVisibility(View.VISIBLE);
-    	editNameLayout.setVisibility(View.VISIBLE);
+    	LinearLayout editLayout = (LinearLayout) findViewById(R.id.NameLayoutEdit);
+    	EditText editText = (EditText) findViewById(R.id.editName);
+    	editText.setVisibility(View.VISIBLE);
+    	editLayout.setVisibility(View.VISIBLE);
     }
-    
-    public void onClickNameButton(View v) {
-    	
-    	LinearLayout editNameLayout = (LinearLayout) findViewById(R.id.NameLayoutEdit);
-    	EditText editNameText = (EditText) findViewById(R.id.editText1);
-    	TextView textViewName = (TextView) findViewById(R.id.textAddName);
 
-    	if(!editNameText.getText().toString().isEmpty()){
-    		textViewName.setText(editNameText.getText());	
+    public void onClickNameButton(View v) {
+
+    	LinearLayout editLayout = (LinearLayout) findViewById(R.id.NameLayoutEdit);
+    	EditText editText = (EditText) findViewById(R.id.editName);
+    	TextView textView = (TextView) findViewById(R.id.textAddName);
+
+    	if(!editText.getText().toString().isEmpty()){
+    		textView.setText(editText.getText());
     	}
-    	editNameText.setVisibility(View.GONE); 
-    	editNameLayout.setVisibility(View.GONE);
+    	editText.setVisibility(View.GONE);
+    	editLayout.setVisibility(View.GONE);
     	//Получаем системную службу - менеджер метода ввода
     	InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
 
     	//Указываем менеджеру в качестве параметра WindowToken нужного EditTextView
-    	imm.hideSoftInputFromWindow(editNameText.getWindowToken(), 0);
+    	imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
     }
-    
-    
+
+    public void onClickPositionText(View v) {
+    	LinearLayout editLayout = (LinearLayout) findViewById(R.id.PositionLayoutEdit);
+    	EditText editText = (EditText) findViewById(R.id.editPosition);
+    	editText.setVisibility(View.VISIBLE);
+    	editLayout.setVisibility(View.VISIBLE);
+    }
+
+    public void onClickPositionButton(View v) {
+
+    	LinearLayout editLayout = (LinearLayout) findViewById(R.id.PositionLayoutEdit);
+    	EditText editText = (EditText) findViewById(R.id.editPosition);
+    	TextView textView = (TextView) findViewById(R.id.textAddPosittion);
+
+    	if(!editText.getText().toString().isEmpty()){
+    		textView.setText(editText.getText());
+    	}
+    	editText.setVisibility(View.GONE);
+    	editLayout.setVisibility(View.GONE);
+    	//Получаем системную службу - менеджер метода ввода
+    	InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+    	//Указываем менеджеру в качестве параметра WindowToken нужного EditTextView
+    	imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+    }
+
+    public void onClickSalaryText(View v) {
+    	LinearLayout editLayout = (LinearLayout) findViewById(R.id.SalaryLayoutEdit);
+    	EditText editText = (EditText) findViewById(R.id.editSalary);
+    	editText.setVisibility(View.VISIBLE);
+    	editLayout.setVisibility(View.VISIBLE);
+    }
+
+    public void onClickSalaryButton(View v) {
+
+    	LinearLayout editLayout = (LinearLayout) findViewById(R.id.SalaryLayoutEdit);
+    	EditText editText = (EditText) findViewById(R.id.editSalary);
+    	TextView textView = (TextView) findViewById(R.id.textAddSalary);
+
+    	if(!editText.getText().toString().isEmpty()){
+    		textView.setText(editText.getText());
+    	}
+    	editText.setVisibility(View.GONE);
+    	editLayout.setVisibility(View.GONE);
+    	//Получаем системную службу - менеджер метода ввода
+    	InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+    	//Указываем менеджеру в качестве параметра WindowToken нужного EditTextView
+    	imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+    }
+
+    public void onClickPhoneText(View v) {
+    	LinearLayout editLayout = (LinearLayout) findViewById(R.id.PhoneLayoutEdit);
+    	EditText editText = (EditText) findViewById(R.id.editPhone);
+    	editText.setVisibility(View.VISIBLE);
+    	editLayout.setVisibility(View.VISIBLE);
+    }
+
+    public void onClickPhoneButton(View v) {
+
+    	LinearLayout editLayout = (LinearLayout) findViewById(R.id.PhoneLayoutEdit);
+    	EditText editText = (EditText) findViewById(R.id.editPhone);
+    	TextView textView = (TextView) findViewById(R.id.textAddPhone);
+
+    	if(!editText.getText().toString().isEmpty()){
+    		textView.setText(editText.getText());
+    	}
+    	editText.setVisibility(View.GONE);
+    	editLayout.setVisibility(View.GONE);
+    	//Получаем системную службу - менеджер метода ввода
+    	InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+    	//Указываем менеджеру в качестве параметра WindowToken нужного EditTextView
+    	imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+    }
+
+public void onClickEmailText(View v) {
+    	LinearLayout editLayout = (LinearLayout) findViewById(R.id.EmailLayoutEdit);
+    	EditText editText = (EditText) findViewById(R.id.editEmail);
+    	editText.setVisibility(View.VISIBLE);
+    	editLayout.setVisibility(View.VISIBLE);
+    }
+
+    public void onClickEmailButton(View v) {
+
+    	LinearLayout editLayout = (LinearLayout) findViewById(R.id.EmailLayoutEdit);
+    	EditText editText = (EditText) findViewById(R.id.editEmail);
+    	TextView textView = (TextView) findViewById(R.id.textAddEmail);
+
+    	if(!editText.getText().toString().isEmpty()){
+    		textView.setText(editText.getText());
+    	}
+    	editText.setVisibility(View.GONE);
+    	editLayout.setVisibility(View.GONE);
+    	//Получаем системную службу - менеджер метода ввода
+    	InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+    	//Указываем менеджеру в качестве параметра WindowToken нужного EditTextView
+    	imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+    }
+
+    public void onClickSubmitCV(View v) {
+    	TextView name = (TextView) findViewById(R.id.textAddName);
+    	TextView birthdate = (TextView) findViewById(R.id.textAddBirth);
+    	TextView position = (TextView) findViewById(R.id.textAddPosittion);
+    	TextView salary = (TextView) findViewById(R.id.textAddSalary);
+    	TextView phone = (TextView) findViewById(R.id.textAddPhone);
+    	TextView email = (TextView) findViewById(R.id.textAddEmail);
+    	String userData = new StringBuilder()
+	    .append("Имя: ").append(name.getText().toString()).append("\n")
+	    .append("Дата рождения: ").append(birthdate.getText().toString()).append("\n")
+	    .append("Должность: ").append(position.getText().toString()).append("\n")
+	    .append("З/П: ").append(salary.getText().toString()).append("\n")
+	    .append("Телефон: ").append(phone.getText().toString()).append("\n")
+	    .append("Email: ").append(email.getText().toString()).append("\n")
+	    .toString();
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	intent.putExtra(EXTRA_MESSAGE, userData);
+    	startActivity(intent);
+    }
+
 }
